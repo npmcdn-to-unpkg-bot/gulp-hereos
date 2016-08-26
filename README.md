3f335cabee8d0b3e8b4f407b32131ed54c78212f
myguddy <seungil.han@gmail.com>
2016-08-26 15:11:56 +0900

    Setup angular2 heroes sample project

---
+++ README.md
@@ -0,0 +1,4 @@
+# gulp-heroes
+> This is a sample angular2 app using gulp and typescript
+Specially source structure is seperated into src and dist
+
---
+++ app/rxjs-extensions.js
@@ -0,0 +1,24 @@
+System.register(['rxjs/add/observable/of', 'rxjs/add/observable/throw', 'rxjs/add/operator/catch', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/distinctUntilChanged', 'rxjs/add/operator/do', 'rxjs/add/operator/filter', 'rxjs/add/operator/map', 'rxjs/add/operator/switchMap'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[
+            function (_1) {},
+            function (_2) {},
+            function (_3) {},
+            function (_4) {},
+            function (_5) {},
+            function (_6) {},
+            function (_7) {},
+            function (_8) {},
+            function (_9) {}],
+        execute: function() {
+        }
+    }
+});
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=rxjs-extensions.js.map
